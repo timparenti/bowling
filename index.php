d391c257194bb14a265fe7c9f09ac0b7eac1a7b2
Tim Parenti <tjtrumpet2323@gmail.com>
2023-07-01 19:14:59 -0400

    Upgrade gtag to Google Analytics 4.

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
   <title>Bowling Scores</title>
   <link rel=stylesheet href="css/bowling.css">
 
-  <!-- Google Analytics -->
+  <!-- Google Analytics added 27 May 2015, updated 1 July 2023 -->
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-9CQ0J7FJR9"></script>
   <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-    ga('create', 'UA-2716332-7', 'timparenti.com');
-    ga('send', 'pageview');
+    gtag('config', 'G-9CQ0J7FJR9');
   </script>
 
 </head>
